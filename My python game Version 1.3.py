0ea9f945217239848d7c53b806a62a9ba1daff6c
Cannooo <31984824+Cannooo@users.noreply.github.com>
2017-09-27 08:08:28 +0100

    Added now

--- My python game Version 1.3.py
+++ My python game Version 1.3.py
@@ -1,26 +1,63 @@
 import random
+import time
 
-hh = random.randint(1,25)
-        
 print("Welcome to my fighting game!")
+print("The attacks are:")
+print(" Punch | 50% chance \n Super Punch | 20% chance \n Heavy | 30% chance")
 
 enemyhealth = 100
+yourhealth = 100
+
 while enemyhealth > 0:
     attack = input("What is your attack? ")
-###Heavy Hit Section###
-    if attack == "heavy":
-        random.randint(1,25)
-        if hh > 15 :
-            enemyhealth = enemyhealth - hh
-            print("HIT","Damage:", hh,"Enemy Health:", enemyhealth)
-            hh = random.randint(1,25)
+
+###Punch Hit Senction###
+    if attack == "Punch":
+        h = random.randint(1,10)
+        random.randint(1,10)
+        if h >= 5 :
+            enemyhealth = enemyhealth - h
+            print("HIT!","Damage:", h, "Enemy Health Remaining:" , enemyhealth)
+            h = random.randint(1,10)
         else:
-            print("MISS", hh, "Enemy Health Remaining:" , enemyhealth)
-            hh = random.randint(1,25)
+            print("MISS! Enemy Health Remaining:" , enemyhealth)
+            h = random.randint(1,10)
+            print("Enemys Turn!")
+            et = ["Heavy", "Punch", "Super Punch"]
+            
+            
+###Punch Hit Senction###
+
 ###Heavy Hit Section###
+    if attack == "Heavy":
+        h = random.randint(1,35)
+        random.randint(1,35)
+        if h >= 20 :
+            enemyhealth = enemyhealth - h
+            print("HIT!","Damage:", h,"Enemy Health:", enemyhealth)
+            h = random.randint(1,35)
+        else:
+            print("MISS!", h, "Enemy Health Remaining:" , enemyhealth)
+            h = random.randint(1,10)
+###Heavy Hit Section###
+
+###Super Punch###
+    if attack == "Super Punch":
+        h = random.randint(1,50)
+        random.randint(1,50)
+        if h >= 40 :
+            enemyhealth = enemyhealth - h
+            print("HIT!","Damage:", h, "Enemy Health Remaining:" , enemyhealth)
+            h = random.randint(1,50)
+        else:
+            print("MISS! Enemy Health Remaining:", enemyhealth)
+            h = random.randint(1,50)
+            print("Enemys Turn!")
+###Super Punch###
+
 else:
     print("You Won")
-
+    
 
 
 
